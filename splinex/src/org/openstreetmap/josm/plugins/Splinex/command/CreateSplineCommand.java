78546a654d67eacc4574a04e2949513e65fdddb7
Kolesár András <kolesar@openstreetmap.hu>
2023-02-05 05:38:56 +0100

    Fix undo in CreateSplineCommand

--- splinex/src/org/openstreetmap/josm/plugins/Splinex/command/CreateSplineCommand.java
+++ splinex/src/org/openstreetmap/josm/plugins/Splinex/command/CreateSplineCommand.java
@@ -1,57 +1,45 @@
 package org.openstreetmap.josm.plugins.Splinex.command;
 
+import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.Command;
+import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.UndoRedoHandler;
 import org.openstreetmap.josm.data.osm.DataSet;
-import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.plugins.Splinex.NodeList;
 import org.openstreetmap.josm.plugins.Splinex.Spline;
 import org.openstreetmap.josm.plugins.Splinex.SplineNode;
 import org.openstreetmap.josm.plugins.Splinex.importer.SchneiderImporter;
 
-import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.openstreetmap.josm.gui.MainApplication.getLayerManager;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-public class CreateSplineCommand extends Command {
+public class CreateSplineCommand extends SequenceCommand {
     private final Spline spline;
     private final NodeList nodes;
     private NodeList previous;
 
-    public CreateSplineCommand(Spline spline, NodeList nodes) {
-        super(getLayerManager().getEditDataSet());
+    public CreateSplineCommand(Spline spline, NodeList nodes, List<Command> cmds) {
+        super(getLayerManager().getEditDataSet(), tr("Create spline"), cmds, false);
         this.spline = spline;
         this.nodes = nodes;
     }
 
     @Override
     public boolean executeCommand() {
+        super.executeCommand();
         previous = spline.nodes;
         spline.nodes = nodes;
-        DataSet dataset = getAffectedDataSet();
-        for (SplineNode sn : nodes) {
-            if (!dataset.containsNode(sn.node)) {
-                dataset.addPrimitive(sn.node);
-            }
-        }
         return true;
     }
 
     @Override
     public void undoCommand() {
         spline.nodes = previous;
-    }
-
-    @Override
-    public void fillModifiedData(Collection<OsmPrimitive> modified, Collection<OsmPrimitive> deleted, Collection<OsmPrimitive> added) {
-        // This command doesn't touches OSM data
-    }
-
-    @Override
-    public String getDescriptionText() {
-        return tr("Create spline");
+        super.undoCommand();
     }
 
     public static void fromSelection(Spline target) {
@@ -61,7 +49,14 @@ public class CreateSplineCommand extends Command {
         if (way == null) return;
         if (way.getNodesCount() < 3) return;
         Spline spline = SchneiderImporter.fromNodes(way.getNodes(), 0.5, way.isClosed());
-        UndoRedoHandler.getInstance().add(new CreateSplineCommand(target, spline.nodes));
+        List<Command> cmds = new LinkedList<>();
+        DataSet dataset = getLayerManager().getEditDataSet();
+        for (SplineNode sn : spline.nodes) {
+            if (!dataset.containsNode(sn.node)) {
+                cmds.add(new AddCommand(dataset, sn.node));
+            }
+        }
+        UndoRedoHandler.getInstance().add(new CreateSplineCommand(target, spline.nodes, cmds));
     }
 
 }
